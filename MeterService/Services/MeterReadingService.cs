b1fab21319becc1deda298b4222c3cbb5df5757e
Mehmet Bergel <bergelmehmet@hotmail.com>
2024-10-29 13:51:08 +0300

    improved code readability and maintainability.

---
+++ MeterService/Services/MeterReadingService.cs
@@ -0,0 +1,20 @@
+﻿using MeterService.Models;
+
+namespace MeterService.Services
+{
+    public class MeterReadingService
+    {
+        public MeterReading CreateMeterReading(MeterReading meterReading)
+        {
+            return new MeterReading
+            {
+                UUID = Guid.NewGuid(),
+                SerialNumber = meterReading.SerialNumber,
+                CurrentValue = meterReading.CurrentValue,
+                LastIndex = meterReading.LastIndex,
+                VoltageValue = meterReading.VoltageValue,
+                ReadingTime = DateTime.Now
+            };
+        }
+    }
+}
